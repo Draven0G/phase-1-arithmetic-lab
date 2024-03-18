b127dda732773b4226ff38323ed39235876a8921
Ramsey <browenbroz@gmail.com>
2024-03-18 21:37:06 +0300

    js6

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let multiply = ('num1 * num2');
+let num1 = 31
+let num2 = 2
+let result = 62
+multiply = result 
 
-// Write your code here
+let mod = ('num3 / num4')
+let num3 = 16
+let num4 = 3
+let answer = 4
+mod = answer
+
+let max = [3,5,7,9,20] 
+max = 20
+
+let random = [1,2,3,4,5]
+random = 1,2,3,4,5
